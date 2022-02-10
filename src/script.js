8613a8f3b78dd35a8c8847538abaa765ee296e0b
mateszucs96 <szucsmate096@gmail.com>
2022-02-10 18:56:05 +0100

    add function to the 'sun' button to switch to moon when clicked, and to take the input value and show it to the screen

--- src/script.js
+++ src/script.js
@@ -1,1 +1,54 @@
-console.log('hello')
+'use strict';
+
+
+
+const switchDarkMode = document.querySelector('.switch-dark-mode');
+const addInput = document.querySelector('.input-container .check-circle');
+const input = document.querySelector('.input-container input');
+const form = document.querySelector('.form-container');
+const todoContainer = document.querySelector('.items-container');
+
+const todos = [];
+
+
+// const html = `
+// <div class="calories">
+//   <h4 class="calories__name">${foods[selected].name}</h4>
+
+//   <h4 class="calories__value">${kalories} kcal</h4>
+// </div>
+// `;
+
+// caloriesContainter.insertAdjacentHTML('afterbegin', html);
+
+console.log(switchDarkMode.src)
+const handleSwitchMode = (e) => {
+    e.target.src = './images/icon-moon.svg';
+}
+
+const addNewToDo = (e) => {
+    //prevent to reload
+    e.preventDefault();
+    // push the input value to the "todos" array
+    todos.push(input.value);
+    const html = `
+        <div class="items-container__item-container list-style">
+            <div class="check-circle"></div>
+            <h4>
+                ${input.value}
+            </h4>
+            <img src="./images/icon-cross.svg" />
+        </div>
+    `
+    todoContainer.insertAdjacentHTML('afterbegin', html);
+
+    //set the input field empty
+    input.value = '';
+
+
+    console.log(todos)
+}
+
+switchDarkMode.addEventListener('click', handleSwitchMode);
+
+form.addEventListener('submit', addNewToDo);
