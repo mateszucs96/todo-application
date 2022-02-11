fda46cf75b87ffd871c9d33ed09e93cbe5a4b2ae
mateszucs96 <szucsmate096@gmail.com>
2022-02-11 20:31:15 +0100

    add the functionality to the cross icon to delete todos and delete the actual todo from the todo array

--- src/script.js
+++ src/script.js
@@ -7,19 +7,11 @@ const addInput = document.querySelector('.input-container .check-circle');
 const input = document.querySelector('.input-container input');
 const form = document.querySelector('.form-container');
 const todoContainer = document.querySelector('.items-container');
+const itemCounter = document.querySelector('.item-counter');
 
 const todos = [];
+let crossIcon = [];
 
-
-// const html = `
-// <div class="calories">
-//   <h4 class="calories__name">${foods[selected].name}</h4>
-
-//   <h4 class="calories__value">${kalories} kcal</h4>
-// </div>
-// `;
-
-// caloriesContainter.insertAdjacentHTML('afterbegin', html);
 
 console.log(switchDarkMode.src)
 const handleSwitchMode = (e) => {
@@ -35,24 +27,48 @@ const addNewToDo = (e) => {
     e.preventDefault();
     // push the input value to the "todos" array
     todos.push(input.value);
-    const html = `
-        <div class="items-container__item-container list-style">
-            <div class="check-circle"></div>
-            <h4>
-                ${input.value}
-            </h4>
-            <img src="./images/icon-cross.svg" />
-        </div>
-    `
-    todoContainer.insertAdjacentHTML('afterbegin', html);
+    displayToDo();
+
+    crossIcon = document.querySelectorAll('.cross-icon');
 
     //set the input field empty
     input.value = '';
 
 
-    console.log(todos)
 }
 
+const displayToDo = () => {
+    const html = `
+    <div class="items-container__item-container list-style">
+        <div class="check-circle"></div>
+        <h4>
+            ${input.value}
+        </h4>
+        <img class="cross-icon" data-number="${todos.length - 1}" src="./images/icon-cross.svg" />
+    </div>
+`
+    todoContainer.insertAdjacentHTML('afterbegin', html);
+
+    // const crossIcon = document.querySelectorAll('.cross-icon');
+    // crossIcon.forEach((el, i) => el.addEventListener('click', () => console.log('clicked')));
+    // console.log(crossIcon);
+}
+
+const deleteToDo = (e) => {
+    if (e.target.classList.contains('cross-icon')) {
+        //remove the element from the todos array
+        const index = todos.indexOf(e.target.parentNode.children[1].textContent.trim());
+        todos.splice(index, 1);
+        //remove the element from the todos list
+        e.target.parentNode.remove();
+    }
+};
+
+todoContainer.addEventListener('click', deleteToDo);
+
+
+
+console.log(crossIcon)
 switchDarkMode.addEventListener('click', handleSwitchMode);
 
 form.addEventListener('submit', addNewToDo);
