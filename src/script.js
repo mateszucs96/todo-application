4a9d695aa42bedefd539b048bfcd1dbb50dbea95
mateszucs96 <szucsmate096@gmail.com>
2022-02-14 23:46:40 +0100

    Change the displayToDo function to make it more flexible and reusable

--- src/script.js
+++ src/script.js
@@ -9,10 +9,15 @@ const form = document.querySelector('.form-container');
 const todoContainer = document.querySelector('.items-container');
 const itemCounter = document.querySelector('.item-counter');
 const headerContainer = document.querySelector('.header-container');
+const selectorContainer = document.querySelector('.selector-container');
 
 const todos = [];
+let todosCopy = [];
+let completedArr = [];
 
-const handleSwitchMode = (e) => {
+
+
+function handleSwitchMode(e) {
     if (e.target.attributes.src.value === './images/icon-sun.svg') {
         e.target.src = './images/icon-moon.svg';
         body.classList.add('light-mode');
@@ -24,14 +29,14 @@ const handleSwitchMode = (e) => {
     }
 }
 
-const updateToDoCounter = () => itemCounter.textContent = `${todos.length} items left`;
+const updateToDoCounter = (arr) => itemCounter.textContent = `${arr.length} items left`;
 
 const addNewToDo = (e) => {
     //prevent to reload
     e.preventDefault();
     // push the input value to the "todos" array
     todos.push(input.value);
-    displayToDo();
+    displayToDo(todos);
 
     //set the input field empty
     input.value = '';
@@ -39,21 +44,33 @@ const addNewToDo = (e) => {
 
 }
 
-const displayToDo = () => {
-    const html = `
-    <div class="items-container__item-container list-style">
-        <div class="check-circle">
-        </div>    
-        <h4>
-            ${input.value}
-        </h4>
-        <img class="cross-icon" data-number="${todos.length - 1}" src="./images/icon-cross.svg" />
-    </div>
-    `
+const displayToDo = (arr) => {
+    todoContainer.textContent = '';
 
-    todoContainer.insertAdjacentHTML('afterbegin', html);
+    arr.forEach((el) => {
+        const type = arr === completedArr ? 'completed' : '';
+        const html = `
+            <div class="items-container__item-container list-style">
+                <div class="check-circle ${type}"></div>
+                <h4>
+                    ${el}
+                </h4>
+                <img class="cross-icon" src="./images/icon-cross.svg" />
+            </div>
+            `
+        todoContainer.insertAdjacentHTML('afterbegin', html);
+        console.log()
+    });
 
-    updateToDoCounter();
+
+    updateToDoCounter(arr);
+};
+
+
+const reRenderToDo = (e) => {
+    todoContainer.textContent = '';
+    console.log(completedArr)
+    displayToDo(completedArr)
 }
 
 const deleteToDo = (e) => {
@@ -66,23 +83,25 @@ const deleteToDo = (e) => {
         todos.splice(index, 1);
         //remove the element from the todos list
         e.target.parentNode.remove();
-        updateToDoCounter();
-        console.log(todos)
+        updateToDoCounter(todos);
+
     }
 };
 
 
 const markItems = (e) => {
-    console.log(e)
+    todosCopy = [...todos];
     if (e.target.parentNode.children[0].classList.contains('completed')) {
         //remove the check mark
         e.target.parentNode.children[0].classList.remove('completed');
         //remove the line-through 
         e.target.parentNode.children[1].style.textDecoration = 'none';
         // remove the element on the todos array
-        const index = todos.indexOf(e.target.parentNode.children[1].textContent.trim());
-        todos.splice(index, 0, e.target.parentNode.children[1].textContent.trim());
-        updateToDoCounter();
+        const index = todosCopy.indexOf(e.target.parentNode.children[1].textContent.trim());
+        completedArr.splice(index, 1);
+        updateToDoCounter(completedArr);
+
+        console.log(todos)
         return;
     }
 
@@ -92,10 +111,11 @@ const markItems = (e) => {
         // add the line-through
         e.target.parentNode.children[1].style.textDecoration = 'line-through';
         //push back the element to the todos array
-        const index = todos.indexOf(e.target.parentNode.children[1].textContent.trim());
-        todos.splice(index, 1);
+        const index = todosCopy.indexOf(e.target.parentNode.children[1].textContent.trim());
+        completedArr.push(...todosCopy.splice(index, 1));
+        updateToDoCounter(todos);
 
-        updateToDoCounter();
+        console.log(todos)
         return;
     }
 
@@ -103,7 +123,7 @@ const markItems = (e) => {
 }
 
 // handle the markItems and deleteToDo on the same container
-function eventHandler(e) {
+const eventHandler = (e) => {
     if (e.target.classList.contains('check-circle')) {
         markItems(e);
     }
@@ -111,6 +131,29 @@ function eventHandler(e) {
         deleteToDo(e);
     }
 }
+
+const setActiveSelector = (e) => {
+    console.log(e.target.textContent)
+    if (e.target.classList.contains('selector-completed')) {
+        Array.from(e.target.parentNode.children).forEach(el => el.classList.remove('active'));
+        e.target.classList.add('active');
+        todoContainer.textContent = '';
+        displayToDo(completedArr.reverse());
+    }
+    if (e.target.classList.contains('selector-all')) {
+        Array.from(e.target.parentNode.children).forEach(el => el.classList.remove('active'));
+        e.target.classList.add('active');
+        todoContainer.textContent = '';
+        displayToDo(todos);
+    }
+    if (e.target.classList.contains('selector-active')) {
+        Array.from(e.target.parentNode.children).forEach(el => el.classList.remove('active'));
+        e.target.classList.add('active');
+        todoContainer.textContent = '';
+        displayToDo(todosCopy);
+    }
+};
+
 
 //Dark and Light mode toggle
 switchDarkMode.addEventListener('click', handleSwitchMode);
@@ -120,3 +163,6 @@ form.addEventListener('submit', addNewToDo);
 
 //Delete ToDo and Mark items handler
 todoContainer.addEventListener('click', eventHandler);
+
+//Selector container active state {
+selectorContainer.addEventListener('click', setActiveSelector);
